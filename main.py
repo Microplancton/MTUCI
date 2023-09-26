f310bd0d61ac7d3bebc94db063107a0e46dffdc3
gordey <gordeyprice@yandex.ru>
2023-09-26 13:55:37 +0300

    Домашняя работа

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
-import pyjokes
-from fastapi import FastAPI
-
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def joke():
-    return pyjokes.get_joke()
-@app.get("/{friend}")
-def friends_joke(friend: str):
-    return friend + " tells his joke:" + pyjokes.get_joke()
-@app.get("/multi/{friend}")
-def multi_friends_joke(friend: str, jokes_number: int):
-    result = ""
-    for i in range(jokes_number):
-     result += friend + f" tells his joke #{i + 1}: " + pyjokes.get_joke() + ""
-    return result
-from pydantic import  BaseModel
-class Joke(BaseModel):
-    friend: str
-    joke: str
-class JokeInput(BaseModel):
-    friend: str
-@app.post("/")
-def create_joke(joke_input: JokeInput):
-    return joke_input.friend + " tells his joke:" + pyjokes.get_joke()
-@app.post("/")
-def create_joke(joke_input: JokeInput):
-    return Joke(friend=joke_input.friend, joke=pyjokes.get_joke())
-@app.post("/", response_model=Joke)
-def create_joke(joke_input: JokeInput):
-    """Создание шутки"""
-    return Joke(friend=joke_input.friend, joke=pyjokes.get_joke())
+import wikipedia 
+from typing import Optional 
+from pydantic import BaseModel 
+from fastapi import FastAPI, Request, status 
+import uvicorn 
+ 
+app = FastAPI() 
+ 
+class Article(BaseModel): 
+title: str 
+ 
+# PATH запрос 
+ 
+@app.get("/article/{title}") 
+def search_by_path(request: Request, title: str): 
+ 
+titleraw = None 
+for title in title.split(): 
+if title != "": 
+titleraw = title 
+articles = 0 
+try: 
+data = wikipedia.summary(titleraw, sentences=4) 
+except wikipedia.exceptions.DisambiguationError as e: 
+data = e.options 
+ 
+return data, status.HTTP_200_OK 
+ 
+# QUERY запрос 
+ 
+"/query_search/" 
+def search_by_query(query: Optional[str] = None): 
+ 
+articles = [wikipedia.search(title) for title in query.split() if title != ""] 
+data = { 
+"articles": [ 
+{"titles": article} 
+for article in articles 
+] 
+} 
+return data, status.HTTP_200_OK 
+ 
+# BODY запрос 
+ 
+"/post_search/" 
+def search_by_body(article: Article): 
+ 
+articles = 0 
+try: 
+data = wikipedia.summary(article, sentences=4) 
+except wikipedia.exceptions.DisambiguationError as e: 
+data = e.options 
+ 
+ 
+return data, status.HTTP_200_OK 
+ 
+if name == "__main__": 
+uvicorn.run("main:app", host="0.0.0.0", port=8000)
